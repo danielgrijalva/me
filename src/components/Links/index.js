02f02b3558f3ffc503cc5a74c5012a322ec85244
danielgrijalva <jdgs.gt@gmail.com>
2019-12-01 02:25:06 -0700

    :art: reorder links

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Icon from "../Icon/";
+import { StyledLinks } from "./styles";
 
 export const Links = () => {
   return (
-    <ul>
-      <Icon icon="twitter" link="https://twitter/danielgrijalvas" />
-      <Icon icon="facebook" link="https://facebook.com/danielgrijalvas/" />
-      <Icon icon="instagram" link="https://instagram.com/danielgrijalvas/" />
-      <Icon icon="flickr" link="https://flickr.com/danielgrijalvas/" />
-      <Icon icon="linkedin" link="https://linkedin.com/in/danielgrijalva/" />
+    <StyledLinks>
       <Icon icon="github" link="https://github.com/danielgrijalva/" />
       <Icon icon="medium" link="https://medium.com/@danielgrijalvas" />
-      <Icon icon="mail" link="mailto:danielgrisoto@gmail.com" />
-      <Icon icon="spotify" link="https://open.spotify.com/user/jdgs.gt" />
+      <Icon icon="steam" link="https://steamcommunity.com/id/jdgsgt/" />
       <Icon icon="letterboxd" link="https://letterboxd.com/danieeel/" />
       <Icon icon="overworld" link="https://overworld.netlify.com/user/daniel" />
-      <Icon icon="steam" link="https://steamcommunity.com/id/jdgsgt/" />
-    </ul>
+      <Icon icon="facebook" link="https://facebook.com/danielgrijalvas/" />
+      <Icon icon="twitter" link="https://twitter/danielgrijalvas" />
+      <Icon icon="linkedin" link="https://linkedin.com/in/danielgrijalva/" />
+      <Icon icon="mail" link="mailto:danielgrisoto@gmail.com" />
+      <Icon icon="spotify" link="https://open.spotify.com/user/jdgs.gt" />
+      <Icon icon="flickr" link="https://flickr.com/danielgrijalvas/" />
+      <Icon icon="instagram" link="https://instagram.com/danielgrijalvas/" />
+    </StyledLinks>
   );
 };
