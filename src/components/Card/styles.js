db86435220c320eab290c26f941aa0d37cf4df1f
daniel grijalva <jdgs.gt@gmail.com>
2020-06-29 12:30:00 -0700

    update styling

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -7,7 +7,7 @@ export const StyledCard = styled.div`
   margin: auto;
   padding: 3em 5em;
   border-radius: 1.25em;
-  background: linear-gradient(25deg, #ff8a80, #f50057);
+  background: linear-gradient(25deg, rgba(2,0,36,.5) 0%, rgba(9,9,121,0.5) 35%, rgba(0,212,255,.5) 100%);
   box-shadow: 0 15px 55px -2px rgba(0, 0, 0, 0.8);
 
   @media (max-width: 768px) {
