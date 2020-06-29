72a8f9af3f1f135528ffe42e4d386e591b207597
daniel grijalva <jdgs.gt@gmail.com>
2020-06-29 11:25:19 -0700

    fix instagram link

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -7,7 +7,7 @@ export const Links = () => {
     <StyledLinks>
       <Icon icon="flickr" link="https://flickr.com/danielgrijalvas/" />
       <Icon icon="github" link="https://github.com/danielgrijalva/" />
-      <Icon icon="instagram" link="https://instagram.com/danie.serdaigle/" />
+      <Icon icon="instagram" link="https://instagram.com/daniel.serdaigle/" />
       <Icon icon="linkedin" link="https://linkedin.com/in/danielgrijalva/" />
       <Icon icon="letterboxd" link="https://letterboxd.com/danieeel/" />
       <Icon icon="mail" link="mailto:danielgrisoto@gmail.com" />
