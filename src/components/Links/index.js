8551ddf348a8cef9063e5aa657ab92f562a58205
daniel grijalva <jdgs.gt@gmail.com>
2020-07-09 02:05:50 -0700

    remove some links

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -10,12 +10,9 @@ export const Links = () => {
       <Icon icon="instagram" link="https://instagram.com/daniel.serdaigle/" />
       <Icon icon="linkedin" link="https://linkedin.com/in/danielgrijalva/" />
       <Icon icon="letterboxd" link="https://letterboxd.com/danieeel/" />
-      <Icon icon="mail" link="mailto:danielgrisoto@gmail.com" />
-      <Icon icon="medium" link="https://medium.com/@danielgrijalvas" />
       <Icon icon="overworld" link="https://overworld.netlify.com/user/daniel" />
       <Icon icon="spotify" link="https://open.spotify.com/user/jdgs.gt" />
       <Icon icon="twitter" link="https://twitter.com/danielgrijalvas" />
-      <Icon icon="upwork" link="https://www.upwork.com/o/profiles/users/~01a2163fcc7909ee40" />
       <Icon icon="vimeo" link="https://vimeo.com/danielgrijalva" />
     </StyledLinks>
   );
