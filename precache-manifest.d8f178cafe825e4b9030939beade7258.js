02129d47ad96f6273442d3450f88467636fd2710
daniel grijalva <jdgs.gt@gmail.com>
2020-07-09 02:27:46 -0700

    Updates

---
+++ precache-manifest.d8f178cafe825e4b9030939beade7258.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cabe9f16768e827fcd0e0e9665d9e25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24f5c47c824a1361e3df",
+    "url": "/static/js/2.ed85641d.chunk.js"
+  },
+  {
+    "revision": "c601ea743ccf71e4a9e4",
+    "url": "/static/js/main.b52f971f.chunk.js"
+  },
+  {
+    "revision": "2d6f5310be8c80c52f1f",
+    "url": "/static/js/runtime-main.96455328.js"
+  }
+]);
